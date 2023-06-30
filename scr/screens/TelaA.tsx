e09d571ad9d3ea379a0063da10a081ea5031c92a
Anabe <99266498+anabmartins@users.noreply.github.com>
2023-06-30 09:39:40 -0300

    Add files via upload

---
+++ scr/screens/TelaA.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export function screens() {
+  return (
+    <View style={styles.container}>
+
+    </View>
+  );
+}
+
+const styles
